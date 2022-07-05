fa139edc92ca1d1d4095ba746515c6b0de7632bd
Brys <35676267+brys0@users.noreply.github.com>
2022-07-04 20:54:31 -0500

    fix (web): Fix mounted cycle props

--- packages/web/src/components/confetti/ZCanvasConfetti.vue
+++ packages/web/src/components/confetti/ZCanvasConfetti.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { onMounted, PropType } from 'vue';
 import confetti, { Options } from 'canvas-confetti';
 import { useConfettiStore } from '../../stores/ConfettiStore';
 const emit = defineEmits(['destroy']);
@@ -7,27 +7,17 @@ const props = defineProps({
     id: String,
     options: Object as PropType<Options>,
 });
-let confettiStore = useConfettiStore();
-
-console.log('Confetti instance created with options ', props.options)
-
-
+const confettiStore = useConfettiStore()
+onMounted(() => {
+    console.log(`[ConfettiFactory -> Create]: ${props.id}`);
+    const instance = confetti.create(document.getElementById(props.id!!) as HTMLCanvasElement, {
+        useWorker: true,
+        resize: true
+    })
+    instance({ ...props.options })?.then(() => confettiStore.confetti.delete(props.id!!));
+})
 </script>
 
 <template>
     <canvas :id="id" :style="{ height: '100%', width: '100%', position: 'absolute', overflow: 'hidden', zIndex: 2000, pointerEvents: 'none' }" />
 </template>
-
-<script lang="ts">
-export default {
-    mounted() {
-        // @ts-ignore
-        const instance = confetti.create(document.getElementById(this.props.id!!) as HTMLCanvasElement, {
-            useWorker: true,
-            resize: true
-        })
-        // @ts-ignore
-        instance({ ...this.props.options })?.then(() => this.confettiStore.confetti.delete(this.props.id!!));
-    }
-}
-</script>
